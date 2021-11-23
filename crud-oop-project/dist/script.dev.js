7ebe4f20da8039632322faf1f5607264ea5a74c2
Krittiya <krittiyaclark@gmail.com>
2021-11-23 11:47:41 -0500

    Creates a static book array and displays book to the UI

--- crud-oop-project/dist/script.dev.js
+++ crud-oop-project/dist/script.dev.js
@@ -1,6 +1,61 @@
-// Book Class: Represent a Book
-// UI Class: Handles UI Tasks
-// Store Class: Handles Storage
-// Event: Display Books
-// Event: Remove a Book
 "use strict";
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// Book Class: Represent a Book
+var Book = function Book(author, title, isbn) {
+  _classCallCheck(this, Book);
+
+  this.author = author;
+  this.title = title;
+  this.isbn = isbn;
+}; // UI Class: Handles UI Tasks
+
+
+var UI =
+/*#__PURE__*/
+function () {
+  function UI() {
+    _classCallCheck(this, UI);
+  }
+
+  _createClass(UI, null, [{
+    key: "displayBooks",
+    value: function displayBooks() {
+      var StoreBooks = [{
+        title: 'Book One',
+        author: 'John Doe',
+        isbn: '12345'
+      }, {
+        title: 'Book Two',
+        author: 'Steven Grinder',
+        isbn: '56789'
+      }];
+      var books = StoreBooks; // Loop the StoreBooks and call the addBookToList method and pass that (book) into it
+
+      books.forEach(function (book) {
+        return UI.addBookToList(book);
+      });
+    }
+  }, {
+    key: "addBookToList",
+    value: function addBookToList(book) {
+      var list = document.querySelector('#book-list'); // Create the row
+
+      var row = document.createElement('tr'); // Add column
+
+      row.innerHTML = "\n        <td>".concat(book.title, "</td>\n        <td>").concat(book.author, "</td>\n        <td>").concat(book.isbn, "</td>\n        <td><a href=\"#\" class=\"btn btn-danger btn-sm delete\">X</a></td>\n        ");
+      list.appendChild(row);
+    }
+  }]);
+
+  return UI;
+}(); // Store Class: Handles Storage
+// Event: Display Books
+
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks); // Event: Remove a Book
--- crud-oop-project/script.js
+++ crud-oop-project/script.js
@@ -1,9 +1,53 @@
 // Book Class: Represent a Book
+class Book {
+	constructor(author, title, isbn) {
+		this.author = author
+		this.title = title
+		this.isbn = isbn
+	}
+}
 
 // UI Class: Handles UI Tasks
+class UI {
+	static displayBooks() {
+		const StoreBooks = [
+			{
+				title: 'Book One',
+				author: 'John Doe',
+				isbn: '12345',
+			},
+			{
+				title: 'Book Two',
+				author: 'Steven Grinder',
+				isbn: '56789',
+			},
+		]
+
+		const books = StoreBooks
+
+		// Loop the StoreBooks and call the addBookToList method and pass that (book) into it
+		books.forEach((book) => UI.addBookToList(book))
+	}
+
+	static addBookToList(book) {
+		const list = document.querySelector('#book-list')
+		// Create the row
+		const row = document.createElement('tr')
+		// Add column
+		row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `
+
+		list.appendChild(row)
+	}
+}
 
 // Store Class: Handles Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 // Event: Remove a Book
